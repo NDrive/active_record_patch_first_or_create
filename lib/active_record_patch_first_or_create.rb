7fe29a453fa4d83602507575a84db01eb3256657
Guilherme Pereira <guilherme.pereira@ndrive.com>
2014-05-07 15:21:41 +0100

    Adds serialized columns

--- lib/active_record_patch_first_or_create.rb
+++ lib/active_record_patch_first_or_create.rb
@@ -3,7 +3,8 @@ require "active_record_patch_first_or_create/version"
 module ActiveRecordPatchFirstOrCreate
   def self.first_or_create relation, attributes
     attributes = attributes.merge(relation.where_values_hash)
-    attributes = attributes.merge(rails_timestamps) if relation.klass.record_timestamps?
+    attributes = attributes.merge(rails_timestamps(relation))
+    attributes = serialize_columns(relation, attributes)
 
     sql = "INSERT INTO #{relation.table_name} (#{attributes.keys.join(",")})
            SELECT #{(['?'] * attributes.values.count).join(', ')}
@@ -14,9 +15,17 @@ module ActiveRecordPatchFirstOrCreate
     relation.first
   end
 
-  def self.rails_timestamps
+  def self.rails_timestamps relation
     time = Time.now
-    { created_at: time, updated_at: time }
+    { created_at: time, updated_at: time }.keep_if { |key| relation.klass.column_names.include?(key.to_s) }
+  end
+
+  def self.serialize_columns relation, attributes
+    Hash[attributes.map do |key, value|
+      serializer = relation.klass.serialized_attributes[key.to_s]
+      value = serializer.dump(value) unless serializer.nil?
+      [key, value]
+    end]
   end
 end
 
