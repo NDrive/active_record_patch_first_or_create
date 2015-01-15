e7b9fcda03ccc1ece1ff0627fe932f3710f77eb2
Guilherme Pereira <guilherme.pereira@ndrive.com>
2015-01-15 15:15:51 +0000

    Rescue RecordNotUnique

--- lib/active_record_patch_first_or_create.rb
+++ lib/active_record_patch_first_or_create.rb
@@ -2,13 +2,18 @@ require "active_record_patch_first_or_create/version"
 require "active_record_patch_first_or_create/arel_query_creator"
 
 module ActiveRecordPatchFirstOrCreate
-  def self.first_or_create relation, attributes
+  def self.first_or_create relation, attributes, tries=nil
 
     attributes = attributes.merge(relation.where_values_hash)
     attributes = attributes.merge(rails_timestamps(relation))
     attributes = serialize_columns(relation, attributes)
 
-    ActiveRecord::Base.connection.execute(ArelQueryCreator.new(relation, attributes).to_sql)
+    begin
+      ActiveRecord::Base.connection.execute(ArelQueryCreator.new(relation, attributes).to_sql)
+    # Rescue first time that gives RecordNotUnique because:
+    # http://www.postgresql.org/message-id/26970.1296761016@sss.pgh.pa.us
+    rescue ActiveRecord::RecordNotUnique
+    end
 
     relation.first
   end
